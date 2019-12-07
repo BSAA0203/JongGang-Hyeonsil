589b95f94e512ea34245cdd08c577a1d82ba127a
BSAA0203 <33346331+BSAA0203@users.noreply.github.com>
2019-12-07 10:58:16 +0900

    클라우드 DB 연동 및 백 버튼 기능 활성화

---
+++ JongGang-Hyeonsil/Assets/Scripts/Move.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Move : MonoBehaviour
+{
+    public void Move_to_cam()
+    {
+        SceneManager.LoadScene("AR_Cam");
+    }
+
+    public void Move_to_list()
+    {
+        SceneManager.LoadScene("List");
+    }
+}
