024f62e55ff027e77a8ca7e2ca602f4985df6278
BSAA0203 <33346331+BSAA0203@users.noreply.github.com>
2019-12-18 16:22:39 +0900

    세로 스크롤 기능 추가

--- JongGang-Hyeonsil/Assets/Scripts/Scroll_imgs.cs
+++ JongGang-Hyeonsil/Assets/Scripts/Scroll_imgs.cs
@@ -1,18 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
 
 public class Scroll_imgs : MonoBehaviour
 {
+    public RawImage Img;
+    public GameObject Scroll;
+    public GameObject Clone_img;
+    string[] FileName;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        FileName = Directory.GetFiles("/storage/emulated/0/DCIM/Imgs/", "*.jpg");
+        for (int i = 0; i < FileName.Length; i++)
+        {
+            cp(i);
+        }
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+
+    void cp(int a)
+    {
+        string myFolderLocation = "/storage/emulated/0/DCIM/Imgs/";
+        DirectoryInfo directory = new DirectoryInfo(myFolderLocation);
+        FileInfo[] file = directory.GetFiles();
+        string myFilename = file[a].Name;
+        string myScreenshotLocation = myFolderLocation + myFilename;
+
+        byte[] pt = File.ReadAllBytes(myScreenshotLocation);
+        Texture2D texture = new Texture2D(Screen.width, Screen.height);
+        texture.LoadImage(pt);
+        Img.texture = texture;
+
+        GameObject Clone_scroll = Instantiate(Clone_img);
+        Clone_scroll.transform.SetParent(Scroll.transform, false);
     }
 }
